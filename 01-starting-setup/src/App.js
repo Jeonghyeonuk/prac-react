2b2512ef60335e859db8fac0d49532b0a060fac3
정현욱 <jeonghyeonuk@jeonghyeonuk-ui-MacBookAir.local>
2023-05-22 22:33:05 +0900

    컴포넌트 분리하기

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -30,22 +30,22 @@ function App() {
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-      ></ExpenseItem>
+      />
       {/* 커스텀 컴포넌트는 반드시 대문자로 시작 하여야 한다.
       (소문자는 HTML 내장 컴포넌트 이기 때문에) */}
     </div>
--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./ExpenseItem.css"; // CSS 파일을 import 하는방법
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  const { title, amount, date } = props;
+  // 상위 컴포넌트(App.js)에서 속성한 값들을 props라는 객체로 받는다.
+  const { title, amount, date } = props; // props객체에서 필요한 값들을 추출한다 이때 변수명은 상위컴포넌트에서 속성에 지정한 이름과 동일해야한다.
   return (
     // class대신 className을 사용해야한다. Class는 자바스크립트 예약어이기 때문에
     <div className="expense-item">
-      <div>{date.toISOString().slice(0, 10)}</div>
       {/* {} 를 이용해서 자바스크립트 코드를 작성 할 수 있다.
        Date는 객체기때문에 문자열로 변환이 필요하다. */}
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
