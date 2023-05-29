d511cc575b7131db14296648b54d8b1063fd0138
정현욱 <jeonghyeonuk@jeonghyeonuk-ui-MacBookAir.local>
2023-05-29 17:50:49 +0900

    색션 4: 리액트 State 및 이벤트다루기 완료

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ExpenseItem.css"; // CSS 파일을 import 하는방법
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  // 상위 컴포넌트(App.js)에서 속성한 값들을 props라는 객체로 받는다.
-  const [title, setTitle] = useState(props.title); //리액트 훅, 반드시 컴포넌트 함수 안에서 호출해야한다 (컴포넌트 함수 밖이나 중첩된 함수안에서 호출불가)
-
-  const onClickHandler = () => {
-    setTitle("updated!");
-    console.log(title);
-  };
-
   return (
     // class대신 className을 사용해야한다. Class는 자바스크립트 예약어이기 때문에
     <Card className="expense-item">
@@ -19,10 +11,9 @@ function ExpenseItem(props) {
        Date는 객체기때문에 문자열로 변환이 필요하다. */}
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={onClickHandler}>change title</button>
     </Card>
   );
 }
--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,33 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem"; // 사용할 컴포넌트를 가져온다.
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 function Expenses(props) {
   const { item } = props;
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const onChangeHandler = (selectedYear) => {
+    console.log(selectedYear);
+    setFilteredYear(selectedYear);
+  };
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={item[0].title}
-        amount={item[0].amount}
-        date={item[0].date}
-      />
-      <ExpenseItem
-        title={item[1].title}
-        amount={item[1].amount}
-        date={item[1].date}
-      />
-      <ExpenseItem
-        title={item[2].title}
-        amount={item[2].amount}
-        date={item[2].date}
-      />
-      <ExpenseItem
-        title={item[3].title}
-        amount={item[3].amount}
-        date={item[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={onChangeHandler}
+        />
+        <ExpenseItem
+          title={item[0].title}
+          amount={item[0].amount}
+          date={item[0].date}
+        />
+        <ExpenseItem
+          title={item[1].title}
+          amount={item[1].amount}
+          date={item[1].date}
+        />
+        <ExpenseItem
+          title={item[2].title}
+          amount={item[2].amount}
+          date={item[2].date}
+        />
+        <ExpenseItem
+          title={item[3].title}
+          amount={item[3].amount}
+          date={item[3].date}
+        />
+      </Card>
+    </div>
   );
 }
 
