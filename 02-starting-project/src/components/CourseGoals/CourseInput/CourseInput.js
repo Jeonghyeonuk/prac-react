2d7a0d8ed15799dc87171d09a9b15010f4295264
정현욱 <jeonghyeonuk@jeonghyeonuk-ui-MacBookAir.local>
2023-06-01 22:25:10 +0900

    styled-componet로 동적 스타일 생성

--- 02-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 02-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,37 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
+
+// styled-component 에서 자기 자신을 가르킬때는 셀렉터를 쓸필요가 없고 자식을가르키거나 수도셀렉터를
+// 사용하기위할때는 & 표시를 쓴다
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+  
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color : ${(props) => (props.invalid ? "red" : "black")}
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+    font: inherit;
+    backgroud-color : ${(props) => (props.invalid ? "#ffd7d7" : "transparent")}
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -26,10 +56,11 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+      {/* <FormControl className={!isValid ? "invalid" : ""}> */}
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
