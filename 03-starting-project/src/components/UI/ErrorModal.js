d53218b34ed459acf9fe1f2be4ac11f94d5c503d
정현욱 <jeonghyeonuk@jeonghyeonuk-ui-MacBookAir.local>
2023-06-04 15:42:16 +0900

    포탈 사용하기

--- 03-starting-project/src/components/UI/ErrorModal.js
+++ 03-starting-project/src/components/UI/ErrorModal.js
@@ -1,24 +1,45 @@
 import React from "react";
+import ReactDom from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
 
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onErrorClose}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onErrorClose}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 function ErrorModal(props) {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onErrorClose}></div>
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onErrorClose}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <>
+      {ReactDom.createPortal(
+        <Backdrop onErrorClose={props.onErrorClose} />,
+        document.querySelector("#backdrop-root")
+      )}
+      {ReactDom.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onErrorClose={props.onErrorClose}
+        />,
+        document.querySelector("#overlay-root")
+      )}
+    </>
   );
 }
 
